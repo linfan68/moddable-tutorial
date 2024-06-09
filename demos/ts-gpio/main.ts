d154f2efbbbab5b1092566f31d6e79ab5fd51dec
Fan Lin <FanL@example.com>
2024-06-09 11:53:38 +0000

    typescript

---
+++ demos/ts-gpio/main.ts
@@ -0,0 +1,23 @@
+trace('=======Blinking LED on GPIO pin 1======\n')
+
+
+const Digital = device.io.Digital
+const led = new Digital({
+   pin: 1,
+   mode: Digital.Output,
+})
+led.write(1)
+
+let state: 0 | 1 = 0
+
+const button = new Digital({
+	pin: 0,
+	mode: Digital.InputPullUp
+})
+
+System.setInterval(() => {
+    const isBlinking = button.read() === 0
+	led.write(isBlinking ? state : 0)
+	state ^= 1
+}, 200)
+
